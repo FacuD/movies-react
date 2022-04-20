c68dd4db007caf59f41be3c080f78a3a666313bd
FacuD <facundodocampo1@gmail.com>
2022-04-20 02:33:57 -0300

    fix: Adding .env

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,11 +1,12 @@
 const api = "https://api.themoviedb.org/3";
 
+console.log(process.env.REACT_APP_BEARER_AUTHORIZATION);
+
 export function get(path) {
   return fetch(api + path, {
     headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYmZmYWYyYzBmZDk2ZGM1MmFjZDQ3YzE1M2Q1Y2Y5ZSIsInN1YiI6IjYyNWNlMmUyODU3MDJlMDA1MWVkZjk3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HWEQvy-zPOD_u-2VkMXMxrFBXCOXFD3zTG7Q3DzTNbI",
-      "Content-Type": "application/json;charset=utf-8",
+      Authorization: `${process.env.REACT_APP_BEARER_AUTHORIZATION}`,
+      "Content-Type": `${process.env.REACT_APP_CONTENT_TYPE}`,
     },
   }).then((response) => response.json());
 }
